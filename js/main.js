8b6d6b8fc9e9aba94c1a9f179ece3dcf94394261
sasa02sasa <sasa02sasa@mail.ru>
2019-04-20 22:05:14 +0600

    Add class of view groups from api, but without styles

--- js/main.js
+++ js/main.js
@@ -6,3 +6,31 @@ const req = new Request({
         'v': '5.95'
     }
 });
+
+
+class VkPinnedGroups {
+    constructor(props) {
+        this.groups = null;
+        this.viewGroups();
+    }
+    async getGroups() {
+        const groups = await req.request();
+        return JSON.parse(groups.response);
+    }
+    async viewGroups() {
+        const groups = await this.getGroups();
+        const wrapperGroups = document.querySelector('.groups');
+        groups.response.forEach(Group => {
+            const wrap = document.createElement('div');
+            wrap.classList.add('group');
+            wrap.innerHTML =
+                `<a href="https://vk.com/${Group.screen_name}" target="_blank">
+                    <img src="${Group.photo_50}"/>
+                    <span>${Group.name}</span>
+                </a>`;
+            wrapperGroups.appendChild(wrap);
+        })
+    }
+}
+
+const VKPinnedGroups = new VkPinnedGroups();
