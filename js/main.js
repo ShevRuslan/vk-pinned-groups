09a0007bd1f68937965cb6fb5f8465542a8bdffc
sasa02sasa <sasa02sasa@mail.ru>
2019-04-20 17:20:19 +0600

    Init object of class Request. Make request to back-end

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-class Request {
-    constructor() {
-        
+const req = new Request({
+    method: 'groups.getById',
+    config: {
+        'group_ids': '76746437,howdyho_net,ovsyanochan,countryballs_re,forwebdev,marvel',
+        'fields': 'links,members_count',
+        'v': '5.95'
     }
-}
+});
